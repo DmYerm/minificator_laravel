d5fd53c8a3c80c8f7b95ae7404de4014114d4a65
DmYerm <167026482+DmYerm@users.noreply.github.com>
2024-04-17 00:32:14 +0300

    hello world version

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -4,9 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 
-class Controller extends BaseController
+class UrlController extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function index(Request $request)
+    {
+        return view('url.index');
+    }
+
+    public function saveLink(Request $request)
+    {
+        return view('url.index');
+    }
 }
